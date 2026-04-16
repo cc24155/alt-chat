bc8075bc0277849d3e76358fc078e9c724227a42
RafaellysSilva <ra12fasilva@gmail.com>
2026-04-15 22:58:16 -0300

    Update actions.ts

--- react/app/Cadastro/actions.ts
+++ react/app/Cadastro/actions.ts
@@ -4,10 +4,15 @@ import bcrypt from "bcryptjs";
 
 export async function createUser(name: string, email: string, user: string, password: string) {
   try {
+<<<<<<< Updated upstream
+=======
+    //autenticação por email
+>>>>>>> Stashed changes
     const { data: authData, error: authError } = await supabase.auth.signUp({
       email,
       password,
       options: {
+<<<<<<< Updated upstream
 
         data: {
           full_name: name,
@@ -30,10 +35,35 @@ export async function createUser(name: string, email: string, user: string, pass
         .insert([
           { 
             id: authData.user.id, 
+=======
+        data: {
+          full_name: name,
+          username: user,
+        },
+      },
+    });
+
+    if (authError) {
+      console.error("Erro no Auth:", authError.message);
+      return { success: false, error: authError.message };
+    }
+
+    //se teve o authData (resposta que deu certo a criação no bd interno do supabase)
+    if (authData && authData.user) {
+      const salt = await bcrypt.genSalt(10);
+      const passwordHash = await bcrypt.hash(password, salt);
+
+      const { data, error: dbError } = await supabase
+        .from('usuario')
+        .insert([
+          { 
+            id: authData.user.id,     //pega o id que está na tabela do supabase e o usa na de usuários
+>>>>>>> Stashed changes
             nome: name, 
             email: email, 
             senha_hash: passwordHash,
             username: user,
+<<<<<<< Updated upstream
             ative: false
           }
         ]);
@@ -52,5 +82,29 @@ export async function createUser(name: string, email: string, user: string, pass
   } catch (e) {
     console.error("Erro crítico:", e);
     return { success: false, error: "Erro inesperado ao criar conta." };
+=======
+            ative: false 
+          }
+        ])
+        .select();      //o select faz com que tenha uma resposta no data
+
+      if (dbError) {
+        console.error("Erro ao inserir na tabela usuario:", dbError.message);
+        return { success: false, error: dbError.message };
+      }
+
+      return { 
+        success: true, 
+        message: "Usuário cadastrado! Verifique seu e-mail.",
+        data: data ? data[0] : null 
+      };
+    }
+
+    return { success: false, error: "Falha ao obter dados de autenticação." };
+
+  } catch (e) {
+    console.error("Erro crítico:", e);
+    return { success: false, error: "Erro interno no servidor." };
+>>>>>>> Stashed changes
   }
 }
--- react/app/Login/actions.ts
+++ react/app/Login/actions.ts
@@ -1,18 +1,12 @@
 "use server"; 
-
-// import { db } from "@/lib/db"; // Seu Prisma, Mongoose, etc.
-
-/*export async function autenticarUsuario(formData: FormData) {
-  const email = formData.get("");
-  const password = formData.get("")
-  
-  // Aqui você faz a mágica no banco de dados
-  // const usuario = await db.user.findUnique({ where: { email } });
-  
-  console.log("Validando no banco de dados...");
-  return { success: true };
-}*/
+import { supabase } from "@/lib/supabase";
 
 export async function autenticarUsuario(user: string, password: string){
-  //const usuario = await db.user.findUnique({ where: { user } });
+  try{
+
+  }
+  catch(e){
+    console.error("Deu erro no servidor: ", e);
+    
+  }
 }
