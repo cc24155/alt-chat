3bc57b491c11ad6dc141db38c5d97af4055b480e
Rafaelly Nascimento <ra12fasilva@gmail.com>
2026-04-23 12:23:57 -0300

    Update page.tsx

--- react/app/Biblioteca/page.tsx
+++ react/app/Biblioteca/page.tsx
@@ -5,12 +5,31 @@ import NavigationBlue from "../components/NavigationBlue";
 import HeroSection from "../components/HeroSection";
 import Contato from "../components/Contato";
 import Footer from "../components/Footer";
+import { EstaLogado } from "../actions";
 
 import { usePictogramas } from "../../arasaac api/arasaac";
 import { PictogramasGrid } from "../components/PictogramaSection";
+import NavBar from "../components/NavBar";
 
 
-export default function BibliotecaPage() {
+export default async function BibliotecaPage() {
+
+  async function Logou() {
+    try{
+      const result = await EstaLogado();
+      if (result?.success){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    catch(e){
+      console.error("Deu erro: ", e);
+      return false;
+    }
+  }
+
   const { q, categorias, resultados, loading } = usePictogramas([
     "Pessoas", "Animais", "Alimentos", "Ações", "Objetos"
   ]);
@@ -25,24 +44,47 @@ export default function BibliotecaPage() {
   if (loading)
     return <div className="text-foreground text-center py-20">Carregando...</div>;
 
+  if (await Logou()){
+    return (
+      <section className="w-full bg-background px-8 py-12 flex flex-col gap-8">
+        <NavigationBlue />
 
-  return (
-    <section className="w-full bg-background px-8 py-12 flex flex-col gap-8">
-      <NavigationBlue />
+        <HeroSection
+          title="Biblioteca"
+          redirectTo="/Biblioteca"
+          showScroll={false}
+        />
 
-      <HeroSection
-        title="Biblioteca"
-        redirectTo="/Biblioteca"
-        showScroll={false}
-      />
+        {/* PICTOGRAMAS */}
+        <div id="resultados">
+          <PictogramasGrid q={q} resultados={resultados} categorias={categorias} />
+        </div>
 
-      {/* PICTOGRAMAS */}
-      <div id="resultados">
-        <PictogramasGrid q={q} resultados={resultados} categorias={categorias} />
-      </div>
+        <Contato />
+        <Footer />
+      </section>
+    );
+  }
+  else{
+    return (
+      <section className="w-full bg-background px-8 py-12 flex flex-col gap-8">
+        <NavBar />
 
-      <Contato />
-      <Footer />
-    </section>
-  );
+        <HeroSection
+          title="Biblioteca"
+          redirectTo="/Biblioteca"
+          showScroll={false}
+        />
+
+        {/* PICTOGRAMAS */}
+        <div id="resultados">
+          <PictogramasGrid q={q} resultados={resultados} categorias={categorias} />
+        </div>
+
+        <Contato />
+        <Footer />
+      </section>
+    );
+  }
+  
 }
