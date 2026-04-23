aa05b24852faa49e62486e8d075eb6e7e6bd2325
RafaellysSilva <ra12fasilva@gmail.com>
2026-04-22 22:47:35 -0300

    pagina de conta pegando os dados do usuario logado.

---
+++ react/app/Conta/actions.ts
@@ -0,0 +1,31 @@
+"use server";
+import { supabase } from "@/lib/supabase";
+
+export async function buscarDadosUsuario() {
+  try {
+    // o auth.getUser() pega o user que logou no navegador q "fica salvo" no localStorage
+    const { data: { user }, error: userError } = await supabase.auth.getUser();
+
+    if (userError || !user) {
+      return { success: false, error: "Usuário não autenticado." };
+    }
+
+    const { data, error } = await supabase  //da tabela usuario pega o username, a biografia e o avatar_url
+      .from('usuario') 
+      .select('username, biografia, avatar_url')
+      .eq('id', user.id)    //pega esses dados todos onde o campo id for igual ao id do user q acabou de logar
+      .single();            //ao invés de acessar os dados como uma array (result.dados[0].username) acessa tipo result.dados.username            
+
+    if (error) {
+      console.error("Erro ao buscar no banco:", error.message);
+      return { success: false, error: "Dados não encontrados." };
+    }
+
+    return { success: true, dados: data };
+
+  } 
+  catch (e) {
+    console.error("Erro: ", e);
+    return { success: false, error: "Erro inesperado." };
+  }
+}
