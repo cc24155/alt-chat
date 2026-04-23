d882a798e2dbeaa85b6d2361173ea69c48d8e439
RafaellysSilva <ra12fasilva@gmail.com>
2026-04-22 22:56:44 -0300

    Update actions.ts

--- react/app/Configuracoes/actions.ts
+++ react/app/Configuracoes/actions.ts
@@ -2,36 +2,8 @@
 import { supabase } from "@/lib/supabase";
 import bcrypt from "bcryptjs";
 
-export async function createUser(name:string, user:string, email:string, bio: string, password: string){
-  try {
-    const salt = await bcrypt.genSalt(10);   //gera dados aleatórios para serem misturados na criptografia em seguida
-    const passwordHash = await bcrypt.hash(password, salt);
+export async function AtualizarDados(newUser:string, newName: string, newEmail:string, newBio:string, newPasword:string) {
+  const {data, error} = await supabase.auth.updateUser({user: newUser})
 
-    const { data, error } = await supabase
-
-    .from('usuario')
-    .update([
-      {
-        nome: name,
-        email: email,
-        senha_hash: passwordHash
-      }
-    ])
-    .select(); //faz com que o data não venha vazio
-
-    if (error) {
-      console.error("Erro ao alterar dados:", error.message);
-      return { success: false, error: error.message };
-    }
-
-    if (data && data.length > 0) {
-      return { success: true, data: data[0] };
-    }
-  
-    return { success: true };
-  }
-  catch(e){
-    console.error("Erro crítico ", e);
-    return {success : false};
-  }
+  //precisa descobrir o id do usuario logado e usar p atualizar as coisas
 }
