6d2d100633f559414971acef90f8e68f71e94a25
RafaellysSilva <ra12fasilva@gmail.com>
2026-08-26 22:59:19 -0300

    funciona o popular banco

--- ia/popular_banco.py
+++ ia/popular_banco.py
@@ -1,8 +1,6 @@
 import time
-import requests
 from supabase import create_client, Client
 from pictogramas import Pictograma
-import supabase
 
 urlSupabase = "https://qtgkewthlpagnbiavxof.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0Z2tld3RobHBhZ25iaWF2eG9mIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTk4MDAxMCwiZXhwIjoyMDkxNTU2MDEwfQ.FEhZAVvA7iEI-wY_ce4UUCQ0vCojfYdATuJ7LPrbYWQ"
@@ -10,16 +8,32 @@ key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0
 supabase: Client = create_client(urlSupabase, key)
 
 def buscarESalvar(idPic):
-    pic = Pictograma()
-    sucesso = pic.buscar_dados(idPic)
-    if not sucesso:
-        print(f"Erro ao buscar pictograma {idPic}.")
-        return
+    pic = Pictograma(idPic)
+    if not pic.buscar_dados():
+        print(f"Pictograma {idPic} nao foi salvo.")
+        return False
+
     dados = {"arasaac_id": pic.id, "palavra": pic.palavra, "classe": pic.classe_gramatical}
 
-    supabase.table("pictograma").upsert(dados).execute()
+    try:
+        supabase.table("pictograma").upsert(dados).execute()
+    except Exception as erro:
+        print(f"Erro ao salvar pictograma {pic.id} no banco: {erro}")
+        return False
+
     print(f"Pictograma {pic.id} ({pic.palavra}) ({pic.classe_gramatical}) salvo!")
+    return True
     
-for i in range(1, 1000):
-    buscarESalvar(i)
-    time.sleep(0.5)  # Atraso de 0,5 segundos entre as requisições
+# for i in range(1, 1000):
+#     try:
+#         buscarESalvar(i)
+#         time.sleep(0.5)  # Atraso de 0,5 segundos entre as requisições
+#     except Exception as e:
+#         print(e)
+if __name__ == "__main__":
+    for i in range(1270, 10000):
+        try:
+            buscarESalvar(i)
+            #time.sleep(0.1)  # Atraso de 0,5 segundos entre as requisições
+        except Exception as e:
+            print(e)
